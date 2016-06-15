60593033c7a2e1ff6271365d755f6ab8a369fa7b
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2016-06-15 14:29:13 -0500

    now properly responding to triggerings via firebase

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 var firebase = require("firebase");
 try {
-	var gpio = require("pi-gpio");
+	var gpio = require("rpi-gpio");
 } catch (err) {
-	console.error("FAILED TO REQUIRE 'pi-gpio' PACKAGE; err:",err,"Are you sure you're on a raspberry pi???");
+	console.error("FAILED TO REQUIRE 'rpi-gpio' PACKAGE; err:",err,"Are you sure you're on a raspberry pi???");
 }
 
-gpio.open(11, "output", function(err){
-	if (err) throw err;
-	gpio.write(11,1, function(err){
-		if (err) throw err;
-		gpio.close(11, function(err){
-			if (err) throw err;
-			console.log("GPIO sequence was a success");
-		});
-	});
-});
+gpio.setup(11, gpio.DIR_OUT);
 
 
 firebase.initializeApp({
@@ -25,7 +16,7 @@ firebase.initializeApp({
 var triggerTimeoutId;
 firebase.database().ref('triggerings').on('child_added', function(snapshot){
 	
-	console.log('snapshot.val()',snapshot.val());
+	//console.log('snapshot.val()',snapshot.val());
 	
 	clearTimeout(triggerTimeoutId);
 	triggerTimeoutId = setTimeout(function(){
@@ -34,7 +25,54 @@ firebase.database().ref('triggerings').on('child_added', function(snapshot){
 
 });
 
+var shutoffTimeoutId;
 function trigger(){
-	console.log("TRIGGERING THE THING!");
+	//console.log("TRIGGERING THE THING!");
 	
+	gpio.write(11,false,function(err){
+		if (err) throw err;
+		//console.log("Turned 11 low, light on");
+		clearTimeout(shutoffTimeoutId);
+		shutoffTimeoutId = setTimeout(function(){
+			shutoff();
+		},1000);
+	});
 }
+
+function shutoff(){
+	//console.log("SHUTTING OFF THE LIGHT");
+	gpio.write(11,true,function(err){
+		if (err) throw err;
+		//console.log("light should be off now");
+	});
+}
+
+
+
+function closePins() {
+    gpio.destroy(function() {
+        console.log('All pins unexported');
+    });
+}
+
+
+
+
+
+process.stdin.resume(); //so the program will not close instantly
+
+function exitHandler(options, err) {
+	closePins();
+    if (options.cleanup) console.log('clean');
+    if (err) console.log(err.stack);
+    if (options.exit) process.exit();
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
