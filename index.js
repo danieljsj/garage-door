2281dac44afede04a116f60b64c0e7fe5d9d5281
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2016-06-15 11:52:15 -0500

    requiring pi-gpio, including error for when you're not on a pi

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var firebase = require("firebase");
+try {
+	var gpio = require("pi-gpio");
+} catch (err) {
+	console.error("FAILED TO REQUIRE 'pi-gpio' PACKAGE; err:",err,"Are you sure you're on a raspberry pi???");
+}
 
 firebase.initializeApp({
   serviceAccount: "auth-config/pi-service-account-credentials-prod.json",
